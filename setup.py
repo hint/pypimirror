c3ba7cbd8a09ade2bdbc49b6e7532a01da193734
centralniak <p.kilczuk@stylesheet.pl>
2011-08-09 15:58:45 +0200

    Bump requirements, bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = desc + '\n\nChanges\n=======\n\n'  + changes
 
 setup(
     name='z3c.pypimirror',
-    version = '1.0.16.1',
+    version = '1.0.16.2',
     author='Daniel Kraft, Andreas Jung et al.',
     author_email='dk@d9t.de',
     description='A module for building a complete or a partial PyPI mirror',
@@ -33,7 +33,7 @@ setup(
     zip_safe=False,
     install_requires = ['setuptools',
                         'zc.lockfile',
-                        'BeautifulSoup<=3.0.9999'],
+                        'BeautifulSoup'],
     extras_require = {
         'test': [ 'zc.buildout',  
                   'zope.testing',
